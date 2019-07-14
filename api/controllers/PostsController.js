a5816fb236e783fbc1236fc555c9bdc72a926d61
marvin evins <maevins@cps.edu>
2019-07-14 15:46:00 -0500

    working with json

--- api/controllers/PostsController.js
+++ api/controllers/PostsController.js
@@ -1,6 +1,18 @@
 module.exports = {
   posts: function(req, res){
-    res.send('here we are hitting the /posts endpoint')
-    
+
+    const post1 = {
+      id:1,
+      title: 'post title 1',
+      body: 'here is my body'}
+
+      const post2 = {
+        id:2,
+        title: 'post title 2',
+        body: 'here is my body 2'}
+
+
+    res.send([post1, post2])
+
   }
 }
